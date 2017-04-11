4d23963678000fcae1eddb86687423541da58cbf
Roman Iuvshin <riuvshin@codenvy.com>
2017-04-12 00:00:13 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -89,8 +89,8 @@ setParentNextDev() {
 
 releaseProject() {
         echo -e "\x1B[92m############### Release: $1\x1B[0m"
-        mvn release:prepare -Dresume=false -Dtag=$2 -DdevelopmentVersion=$3 -DreleaseVersion=$2 "-Darguments=-DskipTests=true -Dskip-validate-sources -Dgpg.passphrase=${GPG_PASSPHRASE}"
-        mvn release:perform "-Darguments=-DskipTests=true -Dskip-validate-sources -Dgpg.passphrase=${GPG_PASSPHRASE}"
+        mvn release:prepare -Dresume=false -Dtag=$2 -DdevelopmentVersion=$3 -DreleaseVersion=$2 "-Darguments=-DskipTests=true -Dskip-validate-sources -Dgpg.passphrase=${GPG_PASSPHRASE} -Darchetype.test.skip=true"
+        mvn release:perform "-Darguments=-DskipTests=true -Dskip-validate-sources -Dgpg.passphrase=${GPG_PASSPHRASE} -Darchetype.test.skip=true"
 }
 
 setCheDashboardTag() {
